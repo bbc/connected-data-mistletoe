b730dda8cbc820a5bbc4fcda29be71a40d00fe24
Bettina Hermant <hermab01@mc-s115446.local>
2018-02-07 13:20:59 +0000

    json answer according to specs

--- app/domain/root.py
+++ app/domain/root.py
@@ -1,8 +1,32 @@
 
 obj = {
-    '$schema': 'https://bramble.api.bbci.co.uk/schema',
-    'total': 6,
-    'limit': 20,
-    'offset': 0,
-    'items': []
+    '$Schema': 'https://bramble.api.bbci.co.uk/schema',
+  	'Id' : '046b6c7f-0b8a-43b9-b35d-6489e6daee91',
+  	'GeneratedBy' : 'REC_CONFIG_0001',
+  	'Recommendations' : [ {
+    	'RecommendedBy' : 'RB_0002',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05d6k3s',
+    	'Medium' : 'video',
+    	'Index' : 0
+  	}, {
+    	'RecommendedBy' : 'RB_0002',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05q56by',
+    	'Medium' : 'video',
+    	'Index' : 1
+  	}, {
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05h72qm',
+    	'Medium' : 'video',
+    	'Index' : 2  
+  	}, {
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p00jy2zh',
+    	'Medium' : 'video',
+    	'Index' : 3
+  	}, {
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p052hx45',
+    	'Medium' : 'video',
+    	'Index' : 4  	
+  	} ]
 }
