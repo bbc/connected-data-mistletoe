afe31ab8b3cefa42952ba93240a645450bf9e2fb
Jana Eggink <egginj01@mc-s108503.local>
2018-02-08 17:01:32 +0000

    first version for rubus, with just small changes copied from bramble

--- app/domain/root.py
+++ app/domain/root.py
@@ -1,26 +1,25 @@
-
 obj = {
-    '$Schema': 'https://bramble.api.bbci.co.uk/schema',
-  	'Id' : '046b6c7f-0b8a-43b9-b35d-6489e6daee91',
-  	'GeneratedBy' : 'REC_CONFIG_0001',
+    '$Schema': 'https://rubus.api.bbci.co.uk/schema',
+  	'Id' : '92fd487d-f164-4276-9d7f-a9ccff8bae9e',
+  	'GeneratedBy' : 'RUBUS_CONFIG_0001',
   	'Recommendations' : [ {
-    	'RecommendedBy' : 'RB_0002',
-    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05d6k3s',
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05h72qm',
     	'Medium' : 'video',
-    	'Index' : 0
+    	'Index' : 0  
   	}, {
-    	'RecommendedBy' : 'RB_0002',
-    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05q56by',
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p00jy2zh',
     	'Medium' : 'video',
     	'Index' : 1
   	}, {
     	'RecommendedBy' : 'CB_0001',
-    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p05h72qm',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p01gnclf',
     	'Medium' : 'video',
-    	'Index' : 2  
+    	'Index' : 2
   	}, {
     	'RecommendedBy' : 'CB_0001',
-    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p00jy2zh',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p004gx5p',
     	'Medium' : 'video',
     	'Index' : 3
   	}, {
@@ -28,5 +27,10 @@ obj = {
     	'ContentURI' : 'http://www.bbc.co.uk/programmes/p052hx45',
     	'Medium' : 'video',
     	'Index' : 4  	
+  	}, {
+    	'RecommendedBy' : 'CB_0001',
+    	'ContentURI' : 'http://www.bbc.co.uk/programmes/p00zhgl5',
+    	'Medium' : 'video',
+    	'Index' : 5  	
   	} ]
 }
